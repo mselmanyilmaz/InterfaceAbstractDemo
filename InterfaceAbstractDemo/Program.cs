f9893b5e5265665d639919d435447428fc78281b
msyilmaz <mselman.yilmaz@spk.gov.tr>
2024-03-06 18:57:04 +0300

    Add project files.

---
+++ InterfaceAbstractDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Concrete;
+using InterfaceAbstractDemo.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceAbstractDemo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        { 
+            BaseCustomerManager customerManager = new NeroCustomerManager();
+            customerManager.Save(new Customer { DateofBirth = new DateTime(1991, 04, 21), FirstName = "Muharrem Selman", LastName = "YILMAZ", NationalityId = "17771400502" });
+        }
+    }
+}
